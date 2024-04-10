962cd83c2e13a88ae83de939fdcbbe9095c7aecc
Eduardo Vasconcelos <>
2024-04-10 14:15:59 -0300

    feat: add finished improper export operator

--- manifest/manifest_handler.py
+++ manifest/manifest_handler.py
@@ -1,5 +1,12 @@
+from enum import Enum
 import os
 from lxml import etree as ET
+
+class ExportedConfig(Enum):
+    EXPORTED_EXPORTED_IS_TRUE = 1
+    EXPORTED_INTENT_FILTER_PRESENT_AND_EXPORTED_NOT_PRESENT = 2
+    NOT_EXPORTED_EXPORTED_IS_FALSE = 3
+    NOT_EXPORTED_INTENT_FILTER_NOT_PRESENT_AND_EXPORTED_NOT_PRESENT = 4
 
 class ManifestHandler:
 
@@ -24,40 +31,90 @@ class ManifestHandler:
             return True
         except ET.ParseError as e:
             return False
+        
+    def getManifestString(self):
+        return ET.tostring(self.manifestXml, pretty_print=True).decode("utf-8")
+        
+    def replaceComponentAttrib(self, newComponent, componentName):
+        query = ".//" + newComponent.tag + "[@android:name='" + componentName + "']"
+        oldComponent = self.manifestXml.find(query, namespaces=self.namespace)
+        for attrib in newComponent.attrib:
+            oldComponent.attrib[attrib] = newComponent.attrib[attrib]
+        
+    def findComponentsWithoutExported(self, type):
+        query = ".//" + type
+        all = []
+        for component in self.manifestXml.findall(query, namespaces=self.namespace):
+            all.append(component)
+        exportedNotPresent = []
+        for component in all:
+            if not component.attrib.get("{" + list(self.namespace.values())[0] + "}exported"):
+                exportedNotPresent.append(component)
+        return exportedNotPresent
 
     def findAllComponents(self, type, exported=None):
         components = []
         if exported == True:
 
             # In order to find exported components, the query is the following: 
-            # 1. exported='true' or (2. intent-filter and 3. exported not present)
+            # 1. exported='true' OR (2. intent-filter AND 3. exported not present)
 
             # 1. exported='true'
             query = ".//" + type + "[@android:exported='true']"
             for component in self.manifestXml.findall(query, namespaces=self.namespace):
-                components.append(component)
+                components.append({
+                        "component": component, 
+                        "name": component.attrib.get("{" + list(self.namespace.values())[0] + "}name"), 
+                        "reason": ExportedConfig.EXPORTED_EXPORTED_IS_TRUE
+                    })
 
             # 3. exported not present
-            query = ".//" + type
-            all = []
-            for component in self.manifestXml.findall(query, namespaces=self.namespace):
-                all.append(component)
-            exportedNotPresent = []
-            for component in all:
-                if not component.attrib.get("{" + list(self.namespace.values())[0] + "}exported"):
-                    exportedNotPresent.append(component)
+            exportedNotPresent = self.findComponentsWithoutExported(type)
 
             # 2. those with intent-filter among the exported not present ones
             for component in exportedNotPresent:
                 if component.find("intent-filter") is not None:
-                    components.append(component)
+                    components.append({
+                            "component": component, 
+                            "name": component.attrib.get("{" + list(self.namespace.values())[0] + "}name"),
+                            "reason": ExportedConfig.EXPORTED_INTENT_FILTER_PRESENT_AND_EXPORTED_NOT_PRESENT
+                        })
             
         elif exported == False:
-            pass
-            # TODO continue here
-        else:
+            
+            # In order to find non-exported components, the query is the following: 
+            # 1. exported='false' OR (2. intent-filter not present AND 3. exported not present)
+
+            # 1. exported='false'
+            query = ".//" + type + "[@android:exported='false']"
             for component in self.manifestXml.findall(query, namespaces=self.namespace):
-                components.append(component)
+                components.append({
+                        "component": component, 
+                        "name": component.attrib.get("{" + list(self.namespace.values())[0] + "}name"),
+                        "reason": ExportedConfig.NOT_EXPORTED_EXPORTED_IS_FALSE
+                    })
+
+            # 3. exported not present
+            exportedNotPresent = self.findComponentsWithoutExported(type)
+
+            # 2. those without intent-filter among the exported not present ones
+            for component in exportedNotPresent:
+                if component.find("intent-filter") is None:
+                    components.append({
+                            "component": component, 
+                            "name": component.attrib.get("{" + list(self.namespace.values())[0] + "}name"),
+                            "reason": ExportedConfig.NOT_EXPORTED_INTENT_FILTER_NOT_PRESENT_AND_EXPORTED_NOT_PRESENT
+                        })
+
+        else:
+            query = ".//" + type
+            for component in self.manifestXml.findall(query, namespaces=self.namespace):
+                components.append({
+                    "component": component,
+                    "name": component.attrib.get("{" + list(self.namespace.values())[0] + "}name"),
+                    "reason": None
+                })
+
         return components
 
     def findAllActivities(self, exported=None):
--- operators/xml/improper_export.py
+++ operators/xml/improper_export.py
@@ -1,3 +1,5 @@
+from manifest.manifest_handler import ExportedConfig
+from random import randrange
 from operators.operators import Operator, OperatorNames, OperatorTypes
 
 class ImproperExport(Operator):
@@ -8,10 +10,41 @@ class ImproperExport(Operator):
     def __init__(self, log):
         super().__init__(log)
 
+    def logNonExportedComponents(self, components):
+        for component in components:
+            self.log.info("Component: %s is not exported. Reason: %s", component["name"], component["reason"])
+
     def mutate(self, destinationPath, manifestHandler):
+        nonExportedComponents = None
         try:
-            applicationComponents = manifestHandler.findAllApplicationComponents(True)
+            nonExportedComponents = manifestHandler.findAllApplicationComponents(exported=False)
+            self.logNonExportedComponents(nonExportedComponents)
         except Exception as e:
             self.log.error("An error occurred while finding all application components: %s. Exiting...", e)
             exit(1)
-        return {}
+
+        self.log.info("Pseudorandomly picking component to mutate...")
+        index = randrange(0, len(nonExportedComponents))
+        component = nonExportedComponents[index]
+        self.log.info("Picked component: %s", component["name"])
+        self.log.info("- tag: %s", component["component"].tag)
+        self.log.info("- attrib: %s", component["component"].attrib)
+
+        self.log.info("Mutating component...")
+        match component["reason"]:
+            case ExportedConfig.NOT_EXPORTED_EXPORTED_IS_FALSE:
+                component["component"].attrib["{" + list(manifestHandler.namespace.values())[0] + "}exported"] = "true"
+            case ExportedConfig.NOT_EXPORTED_INTENT_FILTER_NOT_PRESENT_AND_EXPORTED_NOT_PRESENT:
+                component["component"].attrib["{" + list(manifestHandler.namespace.values())[0] + "}exported"] = "true"
+            case _:
+                self.log.error("Invalid reason for non-exportation: %s", component["reason"])
+                exit(1)
+        
+        self.log.info("Successfully mutated component: %s", component["name"])
+        self.log.info("- tag: %s", component["component"].tag)
+        self.log.info("- attrib: %s", component["component"].attrib)
+        
+        self.log.info("Replacing component in manifest...")
+        manifestHandler.replaceComponentAttrib(component["component"], component["name"])
+        self.log.info("Successfully replaced component in manifest. New manifest is:")
+        self.log.info(manifestHandler.getManifestString())
